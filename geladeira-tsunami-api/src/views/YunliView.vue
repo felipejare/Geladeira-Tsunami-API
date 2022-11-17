513c0899d0f4e26d62154260468a2e8b1e01f804
Felipe "Felepopolo Jaré" Kuhl <99222712+felipejare@users.noreply.github.com>
2022-11-17 15:12:54 -0300

    Add files via upload

--- geladeira-tsunami-api/src/views/YunliView.vue
+++ geladeira-tsunami-api/src/views/YunliView.vue
@@ -8,6 +8,9 @@ export default {
       albums: {},
       artista: {},
       toptrack: {},
+      albumtrack: {},
+      musica: "",
+      musica_mp3: "",
     };
   },
   methods: {
@@ -23,12 +26,26 @@ export default {
       }
       return hashParams;
     },
+    async getPreviewUrl(preview) {
+      const res = await axios.get(preview);
+      console.log("oioioi");
+      console.log(res);
+      return res.data;
+    },
+    async tocar(item) {
+      let response = await axios.get(item.href, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      this.musica_mp3 = response.data;
+      this.musica = item.preview_url;
+    },
   },
   async created() {
     this.token = this.$route.query["access_token"];
 
-
-   let response = await axios.get(
+    let response = await axios.get(
       "	https://api.spotify.com/v1/artists/6ab8tnQvr8sXXIpSZCrbQs",
       {
         headers: {
@@ -39,36 +56,64 @@ export default {
     this.artista = response.data;
 
     response = await axios.get(
-        "https://api.spotify.com/v1/albums/0eX2yFKyUIbC4dhi7Q8jPU",
-        {
-            headers: {
-                Authorization: `Bearer BQADYvgfjlQICS6xcwy1FzgQdHw02b0-TwyX9YwK2ixVQGtqrQY_rDTUdGzOyotcHo-6e7n7nwod_cObkWt2yS2-GZu0MHkwVeGce0FXspODrSS5PHeFeEjVsKN6pPIrgwRz41fLuVQcy_I2cT2YUMdqffanrnc1m5CuW9i-WuTn_ikvju7WrsKGWJ0_Q1pKco2lqVEGLqPZQakyng`
-            },
-        }
+      "https://api.spotify.com/v1/albums/0eX2yFKyUIbC4dhi7Q8jPU",
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
     );
     this.albums = response.data;
+
+    response = await axios.get(
+      "https://api.spotify.com/v1/albums/0eX2yFKyUIbC4dhi7Q8jPU/tracks",
+      {
+        headers: {
+          Authorizartion: `Bearer ${this.token}`,
+        },
+      }
+    );
+    this.albumtrack = response.data;
+
     // console.log(this.getHashParams());
   },
 };
-
-
 </script>
 
 <template>
-    <div>
-        <h2>{{ token }}</h2>
-    </div>
-    coisarada
-    <div class="artista" >
-        {{ artista.genres }}
-        {{ artista.name }}
-        {{ artista.followers }}
-    </div>
-    {{ albums.name }}
-    {{ albums }}
+  <div>
+    <h1>{{ albums.name }}</h1>
+  </div>
+  <div>
+    {{ artista.genres }}
+    {{ artista.name }}
+    {{ artista.followers }}
+    <img :src="albums.images[0].url" width="150" height="200" />
+    <br />
+  </div>
+  <div v-for="item of albums.tracks.items" :key="item.id">
+    <h3>{{ item.name }}</h3>
+    <audio controls>
+      <source :src="item.preview_url" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+    <button @click="tocar(item)">Tocar</button>
+  </div>
+  <!-- <div v-for="item of albums.tracks.items" :key="item.id">
+        <audio controls>
+          <source :src="item.uri" type="audio/mpeg">
+        </audio>
 
+      </div> -->
+  <!-- {{ albumtrack }} -->
 
-
+  <div v-if="musica">
+    <h1>Musica completa</h1>
+    <audio controls>
+      <source :src="musica_mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+  </div>
 </template>
 
 <script></script>
